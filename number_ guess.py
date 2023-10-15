368e7b975532d09fc01880434bc3855b3b43a134
Elena <49128142+Keinara@users.noreply.github.com>
2023-10-15 22:32:32 +0300

    Изминения

--- number_ guess.py
+++ number_ guess.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def game_core_v3(number: int = 1) -> int:
-    """
+    """ Угадай число не более чем за 20 попыток
     Args:
         number (int, optional): Загаданное число. Defaults to 1.
 
